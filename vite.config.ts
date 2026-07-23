22ed0b33fd7201077038c3e49c0c4fb1ebfa51ac
just muadh <moaddona@gmail.com>
2026-07-23 15:52:54 +0100

    Set base to / for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fischer60/', // important for GitHub Pages (https://moadh704.github.io/fischer60/)
+  base: '/', // for Vercel
 })
